74bc8246c83152f15ae34926fc51716b5e77593e
EthanYuan <yuanchengxing@hotmail.com>
2024-09-28 16:34:50 +0800

    remove  check

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -5716,22 +5716,22 @@ impl<SP: Deref> Channel<SP> where
 			if expected_point != PublicKey::from_secret_key(&self.context.secp_ctx, &given_secret) {
 				return Err(ChannelError::close("Peer sent a garbage channel_reestablish with secret key not matching the commitment height provided".to_owned()));
 			}
-			if msg.next_remote_commitment_number > our_commitment_transaction {
-				macro_rules! log_and_panic {
-					($err_msg: expr) => {
-						log_error!(logger, $err_msg, &self.context.channel_id, log_pubkey!(self.context.counterparty_node_id));
-						panic!($err_msg, &self.context.channel_id, log_pubkey!(self.context.counterparty_node_id));
-					}
-				}
-				log_and_panic!("We have fallen behind - we have received proof that if we broadcast our counterparty is going to claim all our funds.\n\
-					This implies you have restarted with lost ChannelMonitor and ChannelManager state, the first of which is a violation of the LDK chain::Watch requirements.\n\
-					More specifically, this means you have a bug in your implementation that can cause loss of funds, or you are running with an old backup, which is unsafe.\n\
-					If you have restored from an old backup and wish to force-close channels and return to operation, you should start up, call\n\
-					ChannelManager::force_close_without_broadcasting_txn on channel {} with counterparty {} or\n\
-					ChannelManager::force_close_all_channels_without_broadcasting_txn, then reconnect to peer(s).\n\
-					Note that due to a long-standing bug in lnd you may have to reach out to peers running lnd-based nodes to ask them to manually force-close channels\n\
-					See https://github.com/lightningdevkit/rust-lightning/issues/1565 for more info.");
-			}
+			// if msg.next_remote_commitment_number > our_commitment_transaction {
+			// 	macro_rules! log_and_panic {
+			// 		($err_msg: expr) => {
+			// 			log_error!(logger, $err_msg, &self.context.channel_id, log_pubkey!(self.context.counterparty_node_id));
+			// 			panic!($err_msg, &self.context.channel_id, log_pubkey!(self.context.counterparty_node_id));
+			// 		}
+			// 	}
+			// 	log_and_panic!("We have fallen behind - we have received proof that if we broadcast our counterparty is going to claim all our funds.\n\
+			// 		This implies you have restarted with lost ChannelMonitor and ChannelManager state, the first of which is a violation of the LDK chain::Watch requirements.\n\
+			// 		More specifically, this means you have a bug in your implementation that can cause loss of funds, or you are running with an old backup, which is unsafe.\n\
+			// 		If you have restored from an old backup and wish to force-close channels and return to operation, you should start up, call\n\
+			// 		ChannelManager::force_close_without_broadcasting_txn on channel {} with counterparty {} or\n\
+			// 		ChannelManager::force_close_all_channels_without_broadcasting_txn, then reconnect to peer(s).\n\
+			// 		Note that due to a long-standing bug in lnd you may have to reach out to peers running lnd-based nodes to ask them to manually force-close channels\n\
+			// 		See https://github.com/lightningdevkit/rust-lightning/issues/1565 for more info.");
+			// }
 		}
 
 		// Before we change the state of the channel, we check if the peer is sending a very old
